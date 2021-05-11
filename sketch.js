0a2c8caa0015ce1207e40ce9bf4ab9a4fa697504
PrathameshDevshatwar12345 <74907997+PrathameshDevshatwar12345@users.noreply.github.com>
2021-05-11 18:26:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
+function setup() {
+	createCanvas(1366, 634);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roofObject=new roof(width/2,70,width+20,20);
+	bob1 = new bob(width/2-160,475,80)
+	bob2 = new bob(width/2-80,475,80)
+	bob3 = new bob(width/2,475,80);
+	bob4 = new bob(width/2+80,475,80);
+	bob5 = new bob(width/2+160,475,80);
+
+	bobObject1=createSprite(320,475,80);
+	
+	rope1 = new rope(bob1.body,roofObject.body,-160, 0)
+	rope2 = new rope(bob2.body,roofObject.body,-80, 0)
+	rope3 = new rope(bob3.body,roofObject.body,0,0);
+	rope4 = new rope(bob4.body,roofObject.body,80,0);
+	rope5 = new rope(bob5.body,roofObject.body,160,0);
+	
+	Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("grey");
+
+  roofObject.display();
+  roofObject.depth=rope1.depth && rope2.depth && rope3.depth && rope4.depth && rope5.depth;
+  roofObject.depth=roofObject.depth+10;
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display(); 
+}
+
+function keyPressed() {
+	if (keyCode===LEFT_ARROW) {
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-500,y:-750});
+	}
+
+	if (keyCode===RIGHT_ARROW) {
+		Matter.Body.applyForce(bob5.body,bob5.body.position,{x:500,y:750});
+	}
+}
